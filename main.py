85070a56b63ef1ff11d926d61f06ecfa20d39623
RML1812 <raditya.lacavi@gmail.com>
2023-12-04 19:18:45 +0700

    Slider always refresh FIXED, Added Floating Search

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 import pandas as pd
 from math import sqrt
 import streamlit as st
+from streamlit_float import *
 from Topsis import Topsis
 
 def display_recommendation(iframe_code, percentage):
@@ -10,6 +11,24 @@ def display_recommendation(iframe_code, percentage):
     st.markdown(iframe_code, unsafe_allow_html=True)
 
 def main():
+    if 'searched' not in st.session_state:
+        st.session_state.searched = False
+
+    if 'clicked' not in st.session_state:
+        st.session_state.clicked = False
+
+    def click_button():
+        st.session_state.clicked = True
+
+    if 'preVal_w' and 'preVal_i' not in st.session_state:
+        st.session_state.preVal_w = [0] * 9
+        st.session_state.preVal_i = [0] * 9
+
+    if 'val_w' and 'val_i' not in st.session_state:
+        st.session_state.val_w = [0] * 9
+        st.session_state.val_i = [0] * 9
+    
+    
     weight = [0] * 9 # create a list of 8 zeros
     impact = [0] * 9 # create a list of 8 zeros
     st.markdown("<h1 style='text-align: center; color: black;'><span style='color: #1DB954;'>Music</span> Recommendation System Using TOPSIS</h1>", unsafe_allow_html=True)    
@@ -36,20 +55,35 @@ def main():
         weight[i] = st.sidebar.slider(descriptions[i], -10, 10, 0)
         impact[i] = 1 if weight[i] >= 0 else 0
         weight[i] = abs(weight[i])
+        st.session_state.preVal_w[i] = weight[i]
+        st.session_state.preVal_i[i] = impact[i]
+    st.sidebar.markdown("***")
 
-    if st.sidebar.button("Search") and weight != [0] * 9:
+
+    float_init()
+    float_container = st.sidebar.container()
+    with float_container:   
+        if weight != [0] * 9:
+            button = float_container.button("Search", on_click=click_button, disabled = False, use_container_width = True)
+        else:
+            error = float_container.error("Please change at least one preference!")
+            button = float_container.button("Search", on_click=click_button, disabled = True, use_container_width = True)
+       
+    float_container.float("bottom: 0; background-color: #f0f2f6; padding: 0px 0px 20px")
+
+    if st.session_state.clicked == True:
         data = pd.read_csv(f'data/{genre}.csv')
-        topsis = Topsis(data, weight, impact)
-        topsis.run()
-        rec = topsis.getEmbed()
-        percent = topsis.getPercentage()
+        topsis = Topsis(data, st.session_state.preVal_w, st.session_state.preVal_i)
+        
 
-        for i, percentage in zip(rec, percent):
-            iframe_code = f'<iframe style="border-radius:12px; background-color: transparent;" src="{i}?utm_source=generator" width="100%" height="160" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-            display_recommendation(iframe_code, percentage)
+        for i in range(0, 9):    
+            st.session_state.val_w[i] = st.session_state.preVal_w[i]
+            st.session_state.val_i[i] = st.session_state.preVal_i[i]
 
-    else:
-        st.sidebar.error("Please change at least one preference!")
+        st.session_state.clicked = False
+        st.session_state.searched = True
+
+    if st.session_state.searched == False:
         st.markdown("<h4 style='text-align: center; color: black;'>↖️ You haven't searched for a song yet, head to the sidebar</h4>", unsafe_allow_html=True)
         st.markdown("<h4 style='text-align: center; color: black;'>How about recommendation of the day 👇</h4>", unsafe_allow_html=True)
         korea = pd.read_csv(f'data/Korea Trimmed.csv')
@@ -58,6 +92,17 @@ def main():
         iframe_code = f'<iframe style="border-radius:12px; background-color: transparent;" src="{embed}?utm_source=generator" width="100%" height="160" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
         st.markdown(f"It's **{sample['trackName'].values[0]}** by **{sample['artistName'].values[0]}**")
         st.markdown(iframe_code, unsafe_allow_html=True)
+    else :
+        data = pd.read_csv(f'data/{genre}.csv')
+        topsis = Topsis(data, st.session_state.val_w, st.session_state.val_i)
+        topsis.run()
+        rec = topsis.getEmbed()
+        percent = topsis.getPercentage()
+
+        for i, percentage in zip(rec, percent):
+            iframe_code = f'<iframe style="border-radius:12px; background-color: transparent;" src="{i}?utm_source=generator" width="100%" height="160" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+            display_recommendation(iframe_code, percentage)
+
 
 if __name__ == '__main__':
     main()
